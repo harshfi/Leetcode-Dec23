50932842efd656be2fefd3b0c9f509f50483e44c
harshfi <86000141+harshfi@users.noreply.github.com>
2023-12-25 17:32:58 +0530

    Time: 4 ms (98.32%) | Memory: 51.5 MB (29.08%) - LeetSync

--- 89-gray-code/gray-code.java
+++ 89-gray-code/gray-code.java
@@ -1,28 +1,13 @@
 class Solution {
     public List<Integer> grayCode(int n) {
-       List< String> greystr=solve(n);
-        List<Integer> ans=new ArrayList();
-        for(String s:greystr)
-        {
-            ans.add(Integer.parseInt(s,2));
+       
+    List<Integer> ans=new ArrayList();
+       for (int i=0; i<(1<<n); i++)
+            ans.add(i^(i>>1));
+
+        
+        return ans;
         }
-        return ans;
+       
     }
-    public List<String> solve (int n)
-    {
-        if(n==1)
-        {
-            List<String> base=new ArrayList();
-            base.add("0");
-            base.add("1");
-            return base;
-        }   
-        List<String> ans=new ArrayList();
-        List<String> temp=solve(n-1);
-        for(int i=0;i<temp.size();i++)
-            ans.add("0"+temp.get(i));
-        for(int i=temp.size()-1;i>=0;i--)
-            ans.add("1"+temp.get(i));
-        return ans;
-    }
-}
+    
