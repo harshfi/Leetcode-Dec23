51356810a63e535b8112253c79fd4fda079a011c
harshfi <86000141+harshfi@users.noreply.github.com>
2023-12-26 20:30:57 +0530

    Time: 18 ms (5.08%) | Memory: 42.9 MB (28.23%) - LeetSync

---
+++ 93-restore-ip-addresses/restore-ip-addresses.java
@@ -0,0 +1,41 @@
+class Solution {
+    List<String> list;
+    public List<String> restoreIpAddresses(String s) {
+        list= new ArrayList<>();
+        form(s,"",0,0);
+        return list;
+        
+    }
+    public void form(String s, String str, int i,int dot){
+
+        if(i==s.length()&& str.length()-4==s.length()&& dot==4) {
+            
+            list.add(str.substring(0,str.length()-1));
+            return;
+        }
+        if(i>=s.length() || str.length()-4>=s.length()|| dot>=4) {
+            System.out.println(str);
+            return;
+        }
+       
+       
+       if(s.charAt(i)=='0'){
+         form(s,str+s.charAt(i)+".",i+1,dot+1);
+       }
+       else{
+
+           form(s,str+s.charAt(i)+".",i+1,dot+1);
+
+          if(i+1<s.length()){
+           form(s,str+s.substring(i,i+2)+".",i+2,dot+1);
+          }
+
+          if(i+2<s.length()){
+              int num =(int)((s.charAt(i)-'0')*100 +(s.charAt(i+1)-'0' )*10 +( s.charAt(i+2)-'0'));
+              if(num<=255){
+                  form(s,str+num+".",i+3,dot+1);
+              }
+          }
+       }
+    }
+}
